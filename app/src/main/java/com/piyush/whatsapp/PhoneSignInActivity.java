10af9e79bce26720d74d028fa11c788a1b37b8a5
Piyush Bhanushali <piyush.bhanushali@peaas.co>
2021-02-19 21:00:32 +0530

    Phone number auth design created

---
+++ app/src/main/java/com/piyush/whatsapp/PhoneSignInActivity.java
@@ -0,0 +1,69 @@
+package com.piyush.whatsapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
+
+import com.piyush.whatsapp.databinding.ActivityPhoneSignInBinding;
+
+public class PhoneSignInActivity extends AppCompatActivity {
+
+    private AlertDialog.Builder builder;
+    private ActivityPhoneSignInBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityPhoneSignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.ccp.registerCarrierNumberEditText(binding.etPhone);
+        findViewById(R.id.btnNext).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(PhoneSignInActivity.this, "Clicked...", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(PhoneSignInActivity.this, ManageOtpActivity.class);
+                intent.putExtra("mobile",binding.ccp.getFullNumberWithPlus().replace(" ",""));
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.login_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.about:
+                builder = new AlertDialog.Builder(this);
+                builder.setTitle("About")
+                        .setMessage("SignUp/SignIn with the number is the process for make your login easy and friendly.\nEnter your phone number to register or login in the WhatsApp")
+                        .setCancelable(true)
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }).show();
+                break;
+        }
+        return true;
+    }
+}
